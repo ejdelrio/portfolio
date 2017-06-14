870e6a76ed5dfcec80aac3d8b6eccc79cfe7a5f7
Edwin Del Rio <edwinjdelrio@gmail.com>
2017-06-14 10:10:44 -0700

    Created constructor that pulls template from html file

---
+++ projects/objectFile.js
@@ -0,0 +1,28 @@
+'use strict'
+
+projectObjects = [
+  {
+    url : 'https://github.com/ejdelrio/portfolio',
+    title: 'Portfolio',
+    body : 'This project is the one that is currently being viewed. It outlines my brief entry into the world of web development. It\'s a current work in progess.',
+    imgSrc : '../styles/img/port.png'
+  },
+  {
+    url : 'https://github.com/ejdelrio/chocolate-pizza',
+    title: 'Chocolate Pizza',
+    body : 'This is my first design composition. It was setup to mimic a food blog wit an interactive checklist that allows users to checkoff ingredients used for certain recipes.',
+    imgSrc : '../styles/img/chocopizza.png'
+  },
+  {
+    url : 'https://github.com/ejdelrio/korn_hub',
+    title: 'Korn Hub',
+    body : 'This was an independant project that I did on the side. It was designed to mimic a well known video website. The site uses youtube URLs to create mock youtube pages. Users can upload a page with s video URL from youtube',
+    imgSrc : '../styles/img/kornhub.png'
+  },
+  {
+    url : 'https://github.com/ejdelrio/Cookie_stand',
+    title: 'Salmon Cookies',
+    body : 'Salmon cookies was meant to be a store front for Pat\'s Salmon Cookies. The site also included some administrative features to track hourly sales of each location. This administrative feature allowed additional tables to be added as well as modification of individual table cells through the web broswer.',
+    imgSrc : '../styles/img/salmon.png'
+  },
+]
